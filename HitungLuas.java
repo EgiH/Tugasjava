6dee8fe5b0d47b872eac19e5c9857f50679ebe20
EgiH <egihermawan91@gmail.com>
2017-01-24 17:44:34 +0700

    Luas Persegi Panjang

---
+++ HitungLuas.java
@@ -0,0 +1,19 @@
+
+package luaspersegipanjang;
+
+public class LuasPersegiPanjang {
+    public static void main (String[] args){
+       
+int panjang = 5;
+int lebar = 4;
+
+float luas;
+System.out.println("panjangnya="+panjang);
+System.out.println("lebarnya="+lebar);
+
+            
+luas =  (float) (panjang*lebar);
+            System.out.println("luasnya adalah="+ panjang*lebar);
+
+    }
+    } 
